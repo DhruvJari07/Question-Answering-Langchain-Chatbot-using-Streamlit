9f6c5608a35eb8f8693202769b96d2d288f7ba04
DhruvJari07 <jariwaladhruv@yahoo.com>
2024-05-15 16:43:22 +0530

    minor chages

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ load_dotenv()
 Huggingface_api = os.getenv("Huggingface_api")
 
 repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
-llm = HuggingFaceEndpoint(repo_id=repo_id, huggingfacehub_api_token='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', temperature=0.3)
+llm = HuggingFaceEndpoint(repo_id=repo_id, huggingfacehub_api_token=Huggingface_api, temperature=0.3)
 
 input_text = 'what is large language model?'
 
